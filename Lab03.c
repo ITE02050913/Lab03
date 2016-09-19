252834a9d15a92a85e374009909ce2332cc03765
ITE02050336 <02050336@me.mcu.edu.tw>
2016-09-19 17:36:07 +0800

    Lab03.c

---
+++ Lab03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10;
+	
+	printf("Please enter ten numbers,and use the spacebar apart:");
+	scanf("%d %d %d %d %d %d %d %d %d %d",&a1,&a2,&a3,&a4,&a5,&a6,&a7,&a8,&a9,&a10);
+	printf("The numbers you enter are:%d %d %d %d %d %d %d %d %d %d \n",a1,a2,a3,a4,a5,a6,a7,a8,a9,a10);
+	return 0;
+}
